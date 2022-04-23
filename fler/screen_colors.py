80da233e474d228bf3a7c5c526c81decb21cba73
Joe <97056884+flerkendroid@users.noreply.github.com>
2022-04-23 05:13:35 -0300

    TERMINAL

---
+++ fler/screen_colors.py
@@ -0,0 +1,21 @@
+##importar a nuestro codigo librerias!
+import os 
+import time
+
+#limpia pantalla 
+
+
+#otras instrucciones dek SO
+
+os.system("pause")
+os.system("color 1A")
+os.system("pause")
+os.system("color 1B")
+os.system("pause")
+os.system("color 4F")
+os.system("pause")
+os.system("color 6D")
+os.system("pause")
+os.system("color 1E")
+os.system("pause")
+os.system("color0F")
